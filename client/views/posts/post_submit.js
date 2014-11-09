d3c5970d68417d67951378c6eee97fb10df8f265
durancristhian@gmail.com <cduran@cduran-pc>
2014-11-09 14:46:51 -0300

    Added actionsPanel + postSubmit functionality

---
+++ client/views/posts/post_submit.js
@@ -0,0 +1,17 @@
+Template.postSubmit.events({
+	
+	"submit form": function (e) {
+		
+		e.preventDefault();
+
+		console.log(e.target);
+
+		var post = {
+			url: $(e.target).find('[name=url]').val(),
+			title: $(e.target).find('[name=title]').val()
+		};
+		
+		post._id = Posts.insert(post);
+		Router.go('postPage', post);
+	}
+});
