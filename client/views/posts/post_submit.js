3f82887139c0a2b4bf37490ab7253ce0cc1344c6
durancristhian@gmail.com <cduran@cduran-pc>
2014-11-11 21:52:20 -0300

    Added validations in postSubmitForm

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -1,3 +1,20 @@
+Template.postSubmit.created = function () {
+
+	Session.set('postSubmitErrors', {});
+};
+
+Template.postSubmit.helpers({
+
+	errorMessage: function(field) {
+
+		return Session.get('postSubmitErrors')[field];
+	},
+	errorClass: function (field) {
+
+		return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
+	}
+});
+
 Template.postSubmit.events({
 	
 	"submit form": function (e) {
@@ -8,6 +25,11 @@ Template.postSubmit.events({
 			title: $(e.target).find('[name=title]').val(),
 			url: $(e.target).find('[name=url]').val()
 		};
+
+		var errors = validatePost(post);
+
+		if (errors.title || errors.url)
+			return Session.set('postSubmitErrors', errors);
 
 		Meteor.call("postInsert", post, function (error, result) {
 
@@ -27,3 +49,16 @@ Template.postSubmit.events({
 		});
 	}
 });
+
+validatePost = function (post) {
+
+	var errors = {};
+
+	if (!post.title)
+		errors.title = "Please fill in a headline";
+
+	if (!post.url)
+		errors.url = "Please fill in a URL";
+
+	return errors;
+}
