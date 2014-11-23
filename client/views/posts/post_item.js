d82025cb4d7201d9e267ea5dc26ba257e3c2cf28
durancristhian@gmail.com <cduran@cduran-pc>
2014-11-23 12:26:59 -0300

    Added comments list + label of comments count

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -9,5 +9,9 @@ Template.postItem.helpers({
 	ownPost: function () {
 		
 		return Meteor.userId() === this.userId;
+	},
+	commentsCount: function () {
+		
+		return Comments.find({postId: this._id}).count();
 	}
 });
