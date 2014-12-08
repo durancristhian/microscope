1dd0c3c7ab6bc7f9b5378e02a6221e64e683c60a
Duran Cristhian <durancristhian@gmail.com>
2014-12-08 13:32:29 -0300

    Chapter 12: Added pagination

--- server/publications.js
+++ server/publications.js
@@ -1,6 +1,17 @@
-Meteor.publish('posts', function() {
+Meteor.publish('posts', function (options) {
 
-	return Posts.find();
+	check(options, {
+		sort: Object,
+		limit: Number
+	});
+
+	return Posts.find({}, options);
+});
+
+Meteor.publish('singlePost', function (id) {
+
+	check(id, String)
+	return Posts.find(id);
 });
 
 Meteor.publish('comments', function () {
@@ -8,7 +19,7 @@ Meteor.publish('comments', function () {
 	return Comments.find();
 });
 
-Meteor.publish('notifications', function() {
+Meteor.publish('notifications', function () {
 
 	return Notifications.find({userId: this.userId, read: false});
 });
